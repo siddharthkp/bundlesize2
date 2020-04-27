9d154e3af9c939d082acbb097daff3996ab7a822
siddharthkp <siddharth.kshetrapal@gmail.com>
2020-04-27 10:39:36 +0200

    change colors

--- github-reporter/_check.js
+++ github-reporter/_check.js
@@ -58,8 +58,18 @@ const run = async ({
   const repo = repositoryPath.split('/')[1]
   console.log('1/4 repo', repositoryPath)
 
+  const parsedText = text
+    .replace(
+      /✔/g,
+      '<img src="https://raw.githubusercontent.com/siddharthkp/bundlesize2/master/github-reporter/public/check-pass.svg"></img>'
+    )
+    .replace(
+      /✖/g,
+      '<img src="https://raw.githubusercontent.com/siddharthkp/bundlesize2/master/github-reporter/public/check-fail.svg"></img>'
+    )
+
   // check output
-  const output = { title, summary, text }
+  const output = { title, summary, text: parsedText }
   console.log('1/4 output', output)
 
   // get installation id for repo
